7ca67458b860d0f9a7776dc1ff1018e627c64890
Kassia Fraga <kassia.fraga@lumaensino.com.br>
2023-10-15 22:00:39 -0300

    Solving problem

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123123", modelo: "Modelo 1", imei: "11111", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo 2", imei: "22222222", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
